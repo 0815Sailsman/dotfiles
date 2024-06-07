2fa850d84c7567ed671ab58ef4818e8c745836d1
saylor <sailsman@tutanota.com>
2024-06-07 12:48:53 +0200

    nvim telescope and zen mode

---
+++ nvim/.config/nvim/lua/saylor/fixes.lua
@@ -0,0 +1,4 @@
+-- theme at the end
+vim.cmd("colorscheme cyberdream")
+-- Transparency in zen mode
+vim.api.nvim_set_hl(0, "ZenBg", { ctermbg = 0 })
--- nvim/.config/nvim/lua/saylor/init.lua
+++ nvim/.config/nvim/lua/saylor/init.lua
@@ -2,7 +2,4 @@ require("saylor.settings")
 require("saylor.lazy")
 require("saylor.cyberdream-config")
 require("saylor.keybinds")
-
-
--- theme at the end
-vim.cmd("colorscheme cyberdream")
+require("saylor.fixes")
