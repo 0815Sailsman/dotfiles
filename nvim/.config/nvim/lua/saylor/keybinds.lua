c7406f83740188273ce8a4ff79e93660bef00547
saylor <sailsman@tutanota.com>
2024-06-07 14:59:53 +0200

    Cycle through buffers with ctrl tab and ctrl shift tab

--- nvim/.config/nvim/lua/saylor/keybinds.lua
+++ nvim/.config/nvim/lua/saylor/keybinds.lua
@@ -1,7 +1,11 @@
 -- save and quit on leader q
 vim.keymap.set("n", "<leader>q", ":x<CR>")
 
---
-
 -- jump to eof with leader k
 vim.keymap.set("n", "<leader>j", ":$<CR>")
+
+-- next buffer
+vim.keymap.set('n', '<C-Tab>', ':bnext<CR>', { noremap = true })
+
+-- previous buffer
+vim.keymap.set('n', '<C-S-Tab>', ':bprevious<CR>', { noremap = true })
